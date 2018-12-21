ddfc780d14ede57a7ce247d050422bb380e3e32b
Ritesh <Ritesh@DESKTOP-1AL1C9U>
2018-12-21 15:15:53 +0530

    update

--- Practice1/src/com/niit/mr3/EmployeeApp.java
+++ Practice1/src/com/niit/mr3/EmployeeApp.java
@@ -8,21 +8,32 @@ public class EmployeeApp {
 
 	public static void main(String[] args) {
 		EmployeeProcessor e = new EmployeeProcessor();
-		Employee e1 = new Employee();
+		Employee e1 = null;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Employee Details - ");
-		System.out.print("Enter id : ");
-		e1.id = sc.nextInt();
-		System.out.print("Enter name : ");
-		e1.name = sc.next();
-		System.out.print("Enter salary : ");
-		e1.salary = sc.nextDouble();
-		e.addEmp(e1);
+		String rep = "";
+		do{
+			e1 = new Employee();
+			System.out.println("Enter New Employee Details - ");
+			System.out.print("Enter id : ");
+			e1.id = sc.nextInt();
+			System.out.print("Enter name : ");
+			e1.name = sc.next();
+			System.out.print("Enter salary : ");
+			e1.salary = sc.nextDouble();
+			e.addEmp(e1);
+//			System.out.println("employee in main : "+e1);;
+//			System.out.println("employees list : ");
+//			e.dispAll();
+			System.out.print("Do you hany more employees ? ");
+			rep=sc.next();
+		}while (!rep.equals("n"));
+
 //		HashMap<Integer , Employee> hm = new HashMap<>();
 		System.out.println("Enter desired option : ");
 		System.out.println("1. Show all");
 		System.out.println("2. Show for given id");
 		int i = sc.nextInt();
+
 		if(i == 1){
 			e.dispAll();
 		}
--- Practice1/src/com/niit/mr3/EmployeeProcessor.java
+++ Practice1/src/com/niit/mr3/EmployeeProcessor.java
@@ -4,20 +4,29 @@ import java.util.HashMap;
 import java.util.Set;
 
 public class EmployeeProcessor {
-	HashMap<Integer , Employee> hm = new HashMap<>();
+	
+	HashMap<Integer , Employee> hm ; 
+	public EmployeeProcessor(){
+		hm = new HashMap<>();
+	}
+	
 	Employee e1 = new Employee();
+	
 	public void addEmp(Employee e){
 		e1 = e;
+//		System.out.println("employee adding : "+e1);
 		hm.put(e1.id, e1);
 		
 	}
+	
 	public void dispAll(){
-		System.out.println("List of employees : ");
+//		System.out.println("List of employees : "+hm);
 		Set<Integer> keys = hm.keySet();
 		for(Integer key : keys){
 			System.out.println(hm.get(key));
 		}	
 	}
+	
 	public void dispEmp(int i){
 		System.out.println(hm.get(i));
 		
